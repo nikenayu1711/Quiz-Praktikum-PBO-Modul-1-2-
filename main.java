3c2cfcf50ad17af44059a8302b355554ab6b468c
nikenayu1711 <82433473+nikenayu1711@users.noreply.github.com>
2021-04-23 14:40:39 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package quiz.praktikum;
+
+
+public class main {
+      public static void main(String[] args) {
+        persegi yes = new persegi();
+        
+        yes.display();
+        yes.persegi();
+    }
+    
+}  
